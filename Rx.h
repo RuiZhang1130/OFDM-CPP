e9fa9e5168bb25141268bd9293396abacc3256fb
rrbb <2379578z@student.gla.ac.uk>
2019-10-04 12:46:24 +0100

    modu?

--- Rx.h
+++ Rx.h
@@ -13,15 +13,22 @@ class Receiver
 {
    public:
    
-   void rece(int n, fftw_complex tr_out[])
+   //void rece(int n, fftw_complex tr_out[])
+	void rece(int n, float modu_out[])
         {
             fftw_plan q;
 	    fftw_complex *in=new fftw_complex[n];
 	    fftw_complex *out=new fftw_complex[n];
             int i;
+	for (i=0; i<n; i++)
+	{
+	in[i][0] = modu_out[2*i];
+	in[i][1] = modu_out[2*i+1];}
 	    // in = tr_out; 
+		
             
-            q = fftw_plan_dft_1d(n, tr_out, out, FFTW_FORWARD, FFTW_ESTIMATE);  //FFT
+            //q = fftw_plan_dft_1d(n, tr_out, out, FFTW_FORWARD, FFTW_ESTIMATE);  //FFT
+	q = fftw_plan_dft_1d(n, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
             fftw_execute(q);
     
             for (i=0; i<n; i++)  // normalize 
