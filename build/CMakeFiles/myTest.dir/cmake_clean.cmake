29bd1ff6d8849ee9819ad5df68493464b6099bca
rrbb <2379578z@student.gla.ac.uk>
2019-10-04 15:41:06 +0100

    build generated by CMakeLists.txt

---
+++ build/CMakeFiles/myTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myTest.dir/main.cpp.o"
+  "myTest.pdb"
+  "myTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
