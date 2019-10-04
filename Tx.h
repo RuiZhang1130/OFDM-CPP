3e796e579c696a53b56f77a797790fc988975f37
rrbb <2379578z@student.gla.ac.uk>
2019-10-04 12:46:41 +0100

    modu?

--- Tx.h
+++ Tx.h
@@ -6,17 +6,21 @@
 #include <stdlib.h> 
 #include <time.h>
 #include <fftw3.h>
+#include <math.h>
 
 using namespace std;
+
 
 class Transmitter
 {
    public:
-   fftw_complex * tran(int n, int data[])
+   //fftw_complex * tran(int n, int data[])
+float * tran(int n, int data[])
          {
 	    fftw_plan p;
 	    fftw_complex *in=new fftw_complex[n];
 	    fftw_complex *out=new fftw_complex[n];
+	    float *modu = new float[2*n];
 
             int i;
             for (i=0; i<n; i++)  //read array
@@ -27,12 +31,21 @@ class Transmitter
             p = fftw_plan_dft_1d(n, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);  //IFFT
             fftw_execute(p);
             
+	    //modulation
+	    for (i=0; i<n; i++)
+	    {
+		modu[2*i] = out[i][0];
+		modu[2*i+1] = out[i][1];
+            }
+	    
+	    
 	    for (i=0; i<n ; i++)
             {
-                cout<<"after IFFT : "<<"element No."<<i<<" "<<out[i][0]<<" "<<out[i][1]<<endl;
+                cout<<"after IFFT : "<<"element No."<<i<<" "<<out[i][0]<<" "<<out[i][1]<<" "<<modu[2*i]<<" "<<modu[2*i+1]<<endl;
 	    }
             fftw_destroy_plan(p);
-	    return out;
+	    //return out;
+	return modu;
 	  }
 };
 
