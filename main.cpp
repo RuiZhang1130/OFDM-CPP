e246f37dd1d8f1340edf9244bb1f91d9dba34a4c
rrbb <2379578z@student.gla.ac.uk>
2019-11-06 22:23:46 +0000

    update init_data

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 #define N 64 //64 bytes
+#define Ns N/2 //symbol_length
 
 int main(void)
 {
@@ -17,19 +18,27 @@ int main(void)
     int i;
 
     srand((unsigned)time(NULL)); //Generate random seeds
-    for(i=0; i<N; i++)
+    
+	/*for(i=0; i<N; i++)
     {
         init_data[i]=(rand()%256)-128; //range:-128~127
         cout <<"element No."<<i<<" "<< init_data[i]<<endl;
-    }
+    }*/
+	
+    for(i=0; i<N; i++)
+	{
+	init_data[i]=rand()%2; //[0,1]
+	cout <<"element No."<<i<<" "<< init_data[i]<<endl;
+	}
     
     Transmitter Tr;
     Receiver Re;
     //fftw_complex *Tr_out;
-	float *Tr_out;
+	/*float *Tr_out;*/
+	fftw_complex *Tr_out=new fftw_complex[Ns];
     
     Tr_out  = Tr.tran(N,init_data);
-    Re.rece(N,Tr_out);
+    Re.rece(Ns,Tr_out);
     
     //fftw_cleanup();
     return 0;
