6f688cbee0b8ffec0ad95b6332f05df148ff9470
rrbb <2379578z@student.gla.ac.uk>
2019-11-15 23:03:58 +0000

    implement all functions

--- TXtoWAV.h
+++ TXtoWAV.h
@@ -15,7 +15,7 @@ class toWAV
 {
    public:
 
-void w_wav(int n, double Tx_out[])
+void w_wav(int n, short Tx_out[])
 {
 	struct wavHead
 	{
@@ -25,28 +25,24 @@ void w_wav(int n, double Tx_out[])
 		char FMT[4];
 		/*long*/ int size1;
 		short int fmttag;
-		short int channel;
+		short int channel;  
 		/*long*/ int samplepersec;
 		/*long*/int bytepersec;
 		short int blockalign;
-		short int bitpersample;
+		short int bitpersample; //16bit
 		char DATA[4];
 		/*long*/ int size2;
 	};
-wavHead head = {{'R','I','F','F'},0,{'W','A','V','E'},{'f','m','t',' '},16,1,1,44100,44100,1,8,{'d','a','t','a'},0};//initialization
-	head.size0=n+16+8;
-	head.size2=n;
+wavHead head = {{'R','I','F','F'},0,{'W','A','V','E'},{'f','m','t',' '},16,1,1,44100,88200,2,16,{'d','a','t','a'},0};//initialization
+	head.size2=2*n;
+	head.size0=(2*n)+44-8;
 
 struct wavHead *point = &head;
-//long int a = sizeof(Tx_out);=n
-	char sup[4] = {'s','u','p','p'};
 
-	//char body[head.size2];
 	FILE *fp;
 	fp = fopen("test.wav","wb");
 	fwrite(point, sizeof(head),1,fp);
-	fwrite(sup,4,1,fp);
-	fwrite(Tx_out, n,8,fp);
+	fwrite(Tx_out, n,1,fp);
 	fclose(fp);
 }
 
