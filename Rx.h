cfd98a1835db5c827015f07b7e405b2c614044b5
rrbb <2379578z@student.gla.ac.uk>
2019-11-15 23:03:16 +0000

    implement all functions

--- Rx.h
+++ Rx.h
@@ -13,79 +13,60 @@ class Receiver
 {
    public:
    
-   //void rece(int n, fftw_complex tr_out[])
-	char * rece(int n, /*float//fftw_complex//char*/double Tx_out[])
+	char * rece(int N_pt, int wav_l, short wav_out[], short pt_start0, short pt_start1, short pt_end0, short pt_end1)
         {
+	int data_cp= N_pt+(N_pt/4);
+	int n = N_pt/2;
+	int data_l = N_pt-4;
+
             fftw_plan q;
 	    fftw_complex *in=new fftw_complex[n];
 	    fftw_complex *out=new fftw_complex[n];
-	//float *demodu = new float[2*n];
-char *demodu = new char[2*n];
-            int i;
-	/*for (i=0; i<n; i++)
-	{
-	in[i][0] = modu_out[2*i];
-	in[i][1] = modu_out[2*i+1];}*/
-	   /* in = Tx_out; */
-		
-            
-            //q = fftw_plan_dft_1d(n, tr_out, out, FFTW_FORWARD, FFTW_ESTIMATE);  //FFT
-	/*q = fftw_plan_dft_1d(n, in, out, FFTW_FORWARD, FFTW_ESTIMATE);
-            fftw_execute(q);*/
-    
-            /*for (i=0; i<n; i++)  // normalize 
-            {
-                out[i][0] *= 1./n; 
-                out[i][1] *= 1./n; 
-            }*/
 
-		/*/////////for (i=0; i<n; i++)
-		{
-			if(out[i][0]>0)
-				demodu[2*i] = 1;
-			else
-				demodu[2*i] = 0;
-		}
-		for (i=0; i<n; i++)
-		{
-			if(out[i][1]>0)
-				demodu[2*i+1] = 1;
-			else
-				demodu[2*i+1] = 0;
-		}///////////////////*/
-    
-           // for (i=0; i<2*n; i++)
-           // {
-                 /*cout<<"after FFT (real part) : "<<"element No."<<i<<" "<<out[i][0]<<endl;*/
-		//cout<<"after FFT : "<<"element No."<<i<<" "<<demodu[i]<<endl;
-           // }
+char *demodu = new char[N_pt];
+char *data = new char[data_l];
+	int i;
+	int j;
+//Positioning pilot
+int pt;
+for(i=0;i<wav_l;i++)
+{
+	if((wav_out[i]==pt_start0)&&(wav_out[i+1]==pt_start1)&&(wav_out[i+N_pt-2]==pt_end0)&&(wav_out[i+N_pt-1]==pt_end1))
+	{
+		pt=i;
+	}
+}
 
 //decode_cp
-int de_cp_l = 2*n/4;
-double *de_cp = new double[2*n];
-for(i=0;i<2*n;i++)
+short *de_cp = new short[N_pt];
+for(i=0;i<N_pt;i++)
 {
-	de_cp[i] = Tx_out[i+de_cp_l];
+	de_cp[i] = wav_out[pt+i];
 }
 
 //demodulation
-int j;
 for(i=0; i<n; i++)
 {
 j=i%2;
-	if(j=0)
+	if(j==0)
 	{
 	in[i][0] = de_cp[2*i];
 	in[i][1] = de_cp[2*i+1];
 	}
-	if(j=1)
+	if(j==1)
 	{
 	in[i][0] = -1*de_cp[2*i];
 	in[i][1] = -1*de_cp[2*i+1];
 	}
 }
+
 q = fftw_plan_dft_1d(n, in, out, FFTW_FORWARD, FFTW_ESTIMATE);//FFT
 fftw_execute(q);
+/*for(i=0;i<10;i++)
+{
+cout<<i<<": "<<out[i][0]<<endl;
+}*/
+
 
 		for (i=0; i<n; i++)
 		{
@@ -101,15 +82,15 @@ fftw_execute(q);
 			else
 				demodu[2*i+1] = 0;
 		}
-//for (i=0; i<4; i++)
-//{
-                 /*cout<<"after FFT (real part) : "<<"element No."<<i<<" "<<out[i][0]<<endl;*/
-//cout<<"after FFT : "<<"element No."<<i<<" "<<in[i][0]<<" "<<in[i][1]<<" "<<demodu[2*i]<<" "<<demodu[2*i+1]<<endl;
-//}
+//rm_pt
+for(i=0;i<data_l;i++)
+{
+	data[i]=demodu[i+2];
+}
 
 
             fftw_destroy_plan(q);   
-	return demodu;
+	return data;
         }
 };
 
