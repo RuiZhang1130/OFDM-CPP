57de5386d5f501d2a21d39b23ba1bd26cbb06854
rrbb <2379578z@student.gla.ac.uk>
2019-11-16 00:35:08 +0000

    Updated comments and style.

--- WAVtoRX.h
+++ WAVtoRX.h
@@ -7,58 +7,56 @@
 #include <time.h>
 #include <fftw3.h>
 
-#include <math.h>
 
 using namespace std;
 
 class WAVto
 {
-   public:
+	public:
 
-int data_l(void)
-{
-	int length;
-	int wav_l;
-	FILE* fp;
-	
-	fp = fopen("record.wav", "rb");
-	fseek(fp, 0, SEEK_END);
-	length=ftell(fp);
-	rewind(fp);
-
-	fclose(fp);
-	wav_l=length/2;
-	return wav_l;
-}
-
-short * r_wav(void)
-{
-	int length;
-	int wav_l;
-	FILE* fp;
-	
-	fp = fopen("record.wav", "rb");
-	fseek(fp, 0, SEEK_END);
-	wav_l=ftell(fp);
-	rewind(fp);
-	wav_l=length/2;
-	short* wavBuffer=(short*)malloc(wav_l* sizeof(short));
-	//short *wavBuffer[wav_l*sizeof(short)];
-	fread(wavBuffer, length, 1, fp);
-	fclose(fp);
-	
-	short *b=(short*)malloc(wav_l* sizeof(short));
-	short *p= wavBuffer;
-
-	for(int i=0;i<wav_l;i++)
+	int data_l(void)
 	{
-		b[i]=(short)*p;
-		p+=1;
+		int length;
+		int wav_l;
+		FILE* fp;
+	
+		fp = fopen("record.wav", "rb");
+		fseek(fp, 0, SEEK_END);
+		length=ftell(fp);
+		rewind(fp);
+
+		fclose(fp);
+		wav_l=length/2; //short int: 2 bytes
+		return wav_l;
 	}
 
-	return b;
-}
+	short * r_wav(void)
+	{
+		int length;
+		int wav_l;
+		FILE* fp;
+	
+		//read record file
+		fp = fopen("record.wav", "rb");
+		fseek(fp, 0, SEEK_END);
+		wav_l=ftell(fp);
+		rewind(fp);
+		wav_l=length/2;
+		short* wavBuffer=(short*)malloc(wav_l* sizeof(short));
+		fread(wavBuffer, length, 1, fp);
+		fclose(fp);
+	
+		short *b=(short*)malloc(wav_l* sizeof(short));
+		short *p= wavBuffer;
 
+		for(int i=0;i<wav_l;i++)
+		{
+			b[i]=(short)*p;
+			p+=1;
+		}
+
+		return b;
+	}
 
 };
 
