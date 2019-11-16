97ad6d941d03a2dc1ded2645006fdf403d2cf4b3
rrbb <2379578z@student.gla.ac.uk>
2019-11-16 00:34:51 +0000

    Updated comments and style.

--- TXtoWAV.h
+++ TXtoWAV.h
@@ -13,41 +13,41 @@ using namespace std;
 
 class toWAV
 {
-   public:
+	public:
 
-void w_wav(int n, short Tx_out[])
-{
-	struct wavHead
+	void w_wav(int n, short Tx_out[])
 	{
-		char RIFF[4];
-		/*long*/ int size0;
-		char WAVE[4];
-		char FMT[4];
-		/*long*/ int size1;
-		short int fmttag;
-		short int channel;  
-		/*long*/ int samplepersec;
-		/*long*/int bytepersec;
-		short int blockalign;
-		short int bitpersample; //16bit
-		char DATA[4];
-		/*long*/ int size2;
-	};
-wavHead head = {{'R','I','F','F'},0,{'W','A','V','E'},{'f','m','t',' '},16,1,1,44100,88200,2,16,{'d','a','t','a'},0};//initialization
-	head.size2=2*n;
-	head.size0=(2*n)+44-8;
+		struct wavHead
+		{
+			char RIFF[4];
+			int size0;
+			char WAVE[4];
+			char FMT[4];
+			int size1;
+			short int fmttag;
+			short int channel;      //1-channel
+			int samplepersec;       //44100Hz
+			int bytepersec;
+			short int blockalign;
+			short int bitpersample; //16bit
+			char DATA[4];
+			int size2;              //length of data
+		};
 
-struct wavHead *point = &head;
+		//initialization
+		wavHead head = {{'R','I','F','F'},0,{'W','A','V','E'},{'f','m','t',' '},16,1,1,44100,88200,2,16,{'d','a','t','a'},0};
+		head.size2=2*n;
+		head.size0=(2*n)+44-8;
 
-	FILE *fp;
-	fp = fopen("test.wav","wb");
-	fwrite(point, sizeof(head),1,fp);
-	fwrite(Tx_out, n,1,fp);
-	fclose(fp);
-}
+		struct wavHead *point = &head;
+		FILE *fp;
+		fp = fopen("test.wav","wb");
+		fwrite(point, sizeof(head),1,fp);
+		fwrite(Tx_out, n,1,fp);
+		fclose(fp);
+	}
 
 
 };
-
 
 #endif
