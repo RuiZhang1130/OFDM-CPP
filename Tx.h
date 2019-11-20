76fbe74be3737ac26267aa37f08c6daf56b5c483
rrbb <2379578z@student.gla.ac.uk>
2019-11-20 00:09:02 +0000

    integration TXtoWAV.h

--- Tx.h
+++ Tx.h
@@ -7,122 +7,206 @@
 #include <time.h>
 #include <fftw3.h>
 #include <math.h>
+#include <vector>
 
+//#include <unistd.h>
 using namespace std;
 
 class Transmitter
 {
 	public:
 
-	short * tran(int n, char data[])
+	void tran(int img_size, int symbol_n)
 	{
-		int i;
-		int pt_l = 2*n+4;   //length after insertion of the pilot tones
-		int pt_e = pt_l-3;  //end pilot position
-		int n2 = pt_l/2;    //length after conversion to complex
+		int k;
+		int k2;
+		int length;
+		FILE* fp;
+	
+		fp = fopen("source01.jpg", "rb"); //read image
+		fseek(fp, 0, SEEK_END);
+		length = ftell(fp);
+		rewind(fp);
+	
+		char* ImgBuffer=(char*)malloc(length* sizeof(char));
+		fread(ImgBuffer, length, 1, fp);
+		fclose(fp);
 
-		fftw_plan p;
-		fftw_complex *in = new fftw_complex[n2];
-		fftw_complex *out = new fftw_complex[n2];
+		int bl = 8*length;      //bit stream length
+		char data[bl];
 
-		double *modu = new double[pt_l];
-		char *data2 = new char[2*n];
-		char *data_pt = new char[pt_l];
-
-		for (i=0; i<2*n; i++)
+		char *b=(char*)malloc(length* sizeof(char));
+		char *p= ImgBuffer;
+	
+		//char->bit
+		for(k=0; k<length; k++)
 		{
-			data2[i] = 2*data[i]-1;  //1->1,0->-1
+			b[k]=(char)*p;
+			data[8*k] = (b[k] & 0x80) >> 7;
+			data[8*k+1] = (b[k] & 0x40) >> 6;
+			data[8*k+2] = (b[k] & 0x20) >> 5;
+			data[8*k+3] = (b[k] & 0x10) >> 4;
+			data[8*k+4] = (b[k] & 0x08) >> 3;
+			data[8*k+5] = (b[k] & 0x04) >> 2;
+			data[8*k+6] = (b[k] & 0x02) >> 1;
+			data[8*k+7] = (b[k] & 0x01);
+			p+=sizeof(char);
 		}
 
-		//Pilot tones
-		for (i=0; i<pt_l; i++)
+		//Symbol
+		int sl = bl/symbol_n;   //each symbol length
+		vector<vector<char> > symbol(symbol_n, vector<char>(sl));
+		for(k=0; k<symbol_n; k++)
 		{
-			if(i<2)
+			for(k2=0; k2<sl; k2++)
 			{
-				data_pt[i] = 2;
-			}
-			else if(i>pt_e)
-			{
-				data_pt[i] = 2;
-			}
-			else
-			{
-				data_pt[i] = data2[i-2];
-			}
-		}
-
-		for (i=0; i<n2; i++)
-		{
-			in[i][0] = data_pt[2*i];
-			in[i][1] = data_pt[2*i+1];
-		}
-
-		p = fftw_plan_dft_1d(n2, in, out, FFTW_BACKWARD, FFTW_ESTIMATE);  //IFFT
-		fftw_execute(p);
-
-		//modulation
-		int j;
-		for (i=0; i<n2; i++)
-		{
-			j=i%2;
-			if(j==0)
-			{
-				modu[2*i] = out[i][0];
-				modu[2*i+1] = out[i][1];
-			}
-			if(j==1)
-			{
-				modu[2*i] = -out[i][0];
-				modu[2*i+1] = -out[i][1];
+				symbol[k][k2] = data[(sl*k)+k2];
 			}
 		}
 
-		//double->short int
-		short *modu_short = new short[pt_l];
-		for (i=0; i<pt_l; i++)
+		char *rand_n = new char[sl];
+		char *data_xor = new char[sl];
+		int N = sl/2;
+		int bbl = 2*sl;	//2*sl		
+		char bb[bbl];
+		fftw_complex *data_qam = new fftw_complex[sl];		
+		fftw_complex *out = new fftw_complex[sl];
+		double *modu = new double[bbl];
+		//short *modu_short = new short[bbl];
+		int cpl = bbl/4;   //1/4 data
+		int tl = bbl+cpl;  //total length
+		double *data_cp = new double[tl];
+		vector<vector<double> > symbol_out(symbol_n, vector<double>(tl));
+		//short *symbol_out = new short[tl];
+
+
+		for(int n=0; n<symbol_n; n++)
 		{
-			modu_short[i]=short(modu[i]);
-		}
-
-		//Pilot tones _value
-		short pt_start0 = modu_short[0];
-		short pt_start1 = modu_short[1];
-		short pt_end0 = modu_short[pt_l-2];
-		short pt_end1 = modu_short[pt_l-1];
-
-		//cyclic prefix
-		int cp_l = pt_l/4; //1/4 data
-		int cp_t = pt_l+cp_l; //total
-		short *cp = new short[cp_t];
-
-		for (i=0;i<cp_t;i++)
-		{
-			if(i<cp_l)
+				
+			//Energy dispersal
+			srand(4);                  //generate random seeds
+			for(k=0; k<sl; k++)
 			{
-				cp[i]=modu_short[i+pt_l-cp_l];
+				rand_n[k] = rand()%2;        //generate a random number of 0 or 1
 			}
-			else
+			for(k=0; k<sl; k++)
 			{
-				cp[i]=modu_short[i-cp_l];
+				data_xor[k] = symbol[n][k]^rand_n[k];   //XOR
+			}
+
+			//*****************************************
+			for(k=0; k<bbl; k++)
+			{
+				bb[k] = 0;
+			}
+			for(k=0; k<sl; k++)
+			{
+				data_xor[k] = 2*data_xor[k]-1;  //1->1,0->-1
+			}
+			
+			for(k=0; k<sl;k++)
+			{
+				bb[N+k] = data_xor[k];
+			}
+			
+			//PT
+			bb[N-2] = 2;
+			bb[N-1] = 0;
+			//bb[bbl-N-2] =2;
+			//bb[bbl-N-1] =0;
+			
+			//QAM
+			for(k=0; k<sl; k++)
+			{
+				data_qam[k][0] = bb[2*k];
+				data_qam[k][1] = bb[2*k+1];
+			}
+	
+			//IFFT ("data_qam" is input)
+			fftw_plan p_ifft;
+			p_ifft = fftw_plan_dft_1d(sl, data_qam, out, FFTW_BACKWARD, FFTW_ESTIMATE);
+			fftw_execute(p_ifft);
+
+	
+			//Bandpass quadrature modulation
+			for (k=0; k<sl; k++)
+			{
+				k2 = k%2;
+				if(k2==0)
+				{
+					modu[2*k] = out[k][0];
+					modu[2*k+1] = out[k][1];
+				}
+				else if(k2==1)
+				{
+					modu[2*k] = -out[k][0];
+					modu[2*k+1] = -out[k][1];
+				}
+			}
+	
+			//Cyclic Prefix	
+			for(k=0; k<tl; k++)
+			{
+				if(k<cpl)
+				{
+					data_cp[k] = modu[k+bbl-cpl];
+				}
+				else
+				{
+					data_cp[k] = modu[k-cpl];
+				}
+			}
+
+			for(k=0; k<tl; k++)
+			{
+				symbol_out[n][k] = data_cp[k];
+			}
+			
+			fftw_destroy_plan(p_ifft);
+		}
+	
+		//Save as WAV
+		struct wavHead
+		{
+			char RIFF[4];
+			int size0;
+			char WAVE[4];
+			char FMT[4];
+			int size1;
+			short int fmttag;
+			short int channel;      //1-channel
+			int samplepersec;       //44100Hz
+			int bytepersec;
+			short int blockalign;
+			short int bitpersample; //32bit
+			char DATA[4];
+			int size2;              //length of data
+		};
+		
+		wavHead head = {{'R','I','F','F'},0,{'W','A','V','E'},{'f','m','t',' '},16,1,1,44100,176400,4,32,{'d','a','t','a'},0};//initialization
+		head.size2 = (8*tl)*symbol_n;
+		head.size0 = (8*tl)*symbol_n+44-8;
+	
+		struct wavHead *point = &head;
+		fp = fopen("test.wav","wb");
+		fwrite(point, sizeof(head), 1, fp);   //write head
+
+		double *wav = new double[symbol_n*tl];
+		vector<double>::iterator it;
+		vector<vector<double>>::iterator iter;
+
+		k = 0;
+		for(iter=symbol_out.begin(); iter!=symbol_out.end(); iter++)
+		{
+			for(it=iter->begin(); it!=iter->end(); it++)
+			{
+				wav[k] = *it;
+				k = k+1;
 			}
 		}
+		fwrite(wav, 8*symbol_n*tl, 1, fp);             //write data
 
-		//add pt_value
-		int pt_cp_l = cp_t+4;
-		short *pt_cp = new short[pt_cp_l];
-
-		pt_cp[0] = pt_start0;
-		pt_cp[1] = pt_start1;
-		pt_cp[2] = pt_end0;
-		pt_cp[3] = pt_end1;
-
-		for (i=4;i<pt_cp_l;i++)
-		{
-			pt_cp[i] = cp[i-4];
-		}
-
-		fftw_destroy_plan(p);
-		return pt_cp;
+		fclose(fp);
 	}
 };
 
